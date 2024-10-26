7cadefa6524b4e3a16a6c65b27eb2dcb8d1160ea
Peter Hedinger <peter.hedinger@gmail.com>
2024-10-26 11:45:56 +0100

    Improve movement

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ use bevy::prelude::*;
 use bevy_pixel_buffer::prelude::*;
 use rand::prelude::*;
 use rand::seq::SliceRandom;
-use std::collections::HashSet;
+use std::collections::HashMap;
 
-const GRID_WIDTH: usize = 640;
-const GRID_HEIGHT: usize = 300;
-const PIXEL_SIZE: usize = 2;
+const GRID_WIDTH: usize = 320;
+const GRID_HEIGHT: usize = 150;
+const PIXEL_SIZE: usize = 4;
 
 // Order is important - lighter at the top
 #[derive(Debug, Clone, Copy, PartialEq)]
-enum Tile {
+enum Material {
     Gas,
     Air,
     Water,
@@ -18,17 +18,21 @@ enum Tile {
     Rock,
 }
 
-fn viscosity(tile: Tile) -> usize {
-    match tile {
-        Tile::Gas => 4,
-        Tile::Air => 3,
-        Tile::Water => 2,
-        Tile::Sand => 1,
-        Tile::Rock => 0
+struct Particle {
+    material: Material
+}
+
+fn viscosity(material: Material) -> usize {
+    match material {
+        Material::Gas => 4,
+        Material::Air => 3,
+        Material::Water => 2,
+        Material::Sand => 1,
+        Material::Rock => 0
     }
 }
 
-fn heavier(a: Tile, b: Tile) -> bool {
+fn heavier(a: Material, b: Material) -> bool {
     a as usize > b as usize
 }
 
@@ -36,7 +40,7 @@ fn heavier(a: Tile, b: Tile) -> bool {
 struct Simulation {
     width: usize,
     height: usize,
-    grid: Vec<Tile>,
+    grid: Vec<Particle>,
     order: Vec<usize>,
 }
 
@@ -45,12 +49,13 @@ impl Simulation {
         let mut rng = rand::thread_rng();
         let grid = (0..width * height)
             .map(|_| match rng.gen_range(0..5) {
-                0 => Tile::Gas,
-                1 => Tile::Air,
-                2 => Tile::Water,
-                3 => Tile::Sand,
-                _ => Tile::Rock,
+                0 => Material::Gas,
+                1 => Material::Air,
+                2 => Material::Water,
+                3 => Material::Sand,
+                _ => Material::Rock,
             })
+            .map(|m| Particle {material: m})
             .collect();
         let mut order: Vec<usize> = (0..width * height).map(|v| v).collect();
         order.shuffle(&mut rng);
@@ -59,13 +64,13 @@ impl Simulation {
 
     fn update(&mut self) {
         let mut rng = rand::thread_rng();
-        let mut moved = HashSet::new();
+        let mut moved = HashMap::new();
         for order_idx in 0..self.order.len() {
             self.update_tile(order_idx, &mut rng, &mut moved);
         }
     }
 
-    fn update_tile(&mut self, order_idx: usize, rng: &mut ThreadRng, moved: &mut HashSet<usize>) {
+    fn update_tile(&mut self, order_idx: usize, rng: &mut ThreadRng, moved: &mut HashMap<usize, usize>) {
         // Re-compute the x/y of the chosen location
         let idx = self.order[order_idx];
         let x = idx % self.width;
@@ -78,50 +83,77 @@ impl Simulation {
         let choice = rng.gen_ratio(1, 2);
 
         // 0,0 is top left
-        let tile = self.grid[idx];
-        if y < self.height - 1 && heavier(tile, self.grid[idx_below]) {
-            self.try_swap(idx, idx_below, moved);
-
-        } else if viscosity(tile) > 1 {
+        let particle = &self.grid[idx];
+        let material = particle.material;
+        let mut particle_moved = false;
+        if y < self.height - 1 && heavier(material, self.grid[idx_below].material) {
+            particle_moved = self.try_swap(idx, idx_below, moved);
+        }
+        
+        if !particle_moved && viscosity(material) > 1 {
             if choice && x > 0 {
-                let tile_left = self.grid[idx_left];
-                if viscosity(tile_left) > 1 && heavier(self.grid[idx_left], tile) {
-                    self.try_swap(idx, idx_left, moved);
+                let material_left = self.grid[idx_left].material;
+                if viscosity(material_left) > 1 && heavier(material_left, material) {
+                    particle_moved =self.try_swap(idx, idx_left, moved);
                 }
             } else if !choice && x < (self.width - 1) {
-                let tile_right = self.grid[idx_right];
-                if viscosity(tile_right) > 1 && heavier(self.grid[idx_right], tile) {
-                    self.try_swap(idx, idx_right, moved);
+                let material_right = self.grid[idx_right].material;
+                if viscosity(material_right) > 1 && heavier(material_right, material) {
+                    particle_moved =self.try_swap(idx, idx_right, moved);
                 }
             }
+        }
 
-        } else if y < (self.height - 1) {
+        if !particle_moved && y < (self.height - 1) {
             if choice && x > 0 {
-                let tile_left = self.grid[idx_left];
+                let material_left = self.grid[idx_left].material;
                 let idx_below_left = idx_below - 1;
-                let tile_below_left = self.grid[idx_below_left];
-                if heavier(tile, tile_left) && heavier(tile, tile_below_left) {
-                    self.try_swap(idx, idx_left, moved);
+                let material_below_left = self.grid[idx_below_left].material;
+                if heavier(material, material_left) && heavier(material, material_below_left) {
+                    particle_moved = self.try_swap(idx, idx_left, moved);
                 }
             } else if !choice && x < (self.width - 1) {
-                let tile_right = self.grid[idx_right];
+                let material_right = self.grid[idx_right].material;
                 let idx_below_right = idx_below + 1;
-                let tile_below_right = self.grid[idx_below_right];
-                if heavier(tile, tile_right) && heavier(tile, tile_below_right) {
-                    self.try_swap(idx, idx_right, moved);
+                let material_below_right = self.grid[idx_below_right].material;
+                if heavier(material, material_right) && heavier(material, material_below_right) {
+                    particle_moved = self.try_swap(idx, idx_right, moved);
+                }
+            }
+        }
+
+        if !particle_moved && y > 0 {
+            let idx_above = idx - self.width;
+            let material_above = self.grid[idx_above].material;
+            if choice && x > 0 {
+                let material_left = self.grid[idx_left].material;
+                if heavier(material, material_left) && heavier(material_above, material_left) {
+                    particle_moved = self.try_swap(idx, idx_left, moved);
+                }
+            } else if !choice && x < (self.width - 1) {
+                let material_right = self.grid[idx_right].material;
+                if heavier(material, material_right) && heavier(material_above, material_right) {
+                    particle_moved = self.try_swap(idx, idx_right, moved);
                 }
             }
         }
     }
 
-    fn try_swap(&mut self, idx_a: usize, idx_b: usize, moved: &mut HashSet<usize>) {
-        if self.grid[idx_a] == Tile::Rock || self.grid[idx_b] == Tile::Rock {
-            return;
+    fn try_swap(&mut self, idx_a: usize, idx_b: usize, moved: &mut HashMap<usize, usize>) -> bool {
+        if self.grid[idx_a].material == Material::Rock || self.grid[idx_b].material == Material::Rock {
+            return false;
         }
-        if !moved.contains(&idx_a) && !moved.contains(&idx_b) {
+        let moved_a = match moved.get(&idx_a) { Some(c) => *c, None => 0 };
+        let moved_b = match moved.get(&idx_b) { Some(c) => *c, None => 0 };
+        let viscocity_a = viscosity(self.grid[idx_a].material);
+        let viscocity_b = viscosity(self.grid[idx_a].material);
+        if moved_a < viscocity_a && moved_b < viscocity_b {
             self.grid.swap(idx_a, idx_b);
-            moved.insert(idx_a);
-            moved.insert(idx_b);
+            moved.insert(idx_a, moved_a + 1);
+            moved.insert(idx_b, moved_b + 1);
+            true
+        } else {
+            false
         }
     }
 
@@ -129,16 +161,16 @@ impl Simulation {
         let y: usize = pos.y.try_into().unwrap();
         let x: usize = pos.x.try_into().unwrap();
         let idx: usize = y * self.width + x;
-        self.get_tile_color(self.grid[idx])
+        self.get_tile_color(self.grid[idx].material)
     }
 
-    fn get_tile_color(&self, tile: Tile) -> Color {
+    fn get_tile_color(&self, tile: Material) -> Color {
         match tile {
-            Tile::Gas => Color::srgba(0.2, 0.8, 0.1, 0.8),
-            Tile::Air => Color::srgba(0.0, 0.0, 0.0, 0.0),
-            Tile::Water => Color::srgba(0.0, 0.0, 1.0, 0.8),
-            Tile::Sand => Color::srgba(1.0, 1.0, 0.1, 0.8),
-            Tile::Rock => Color::srgba(1.0, 1.0, 1.0, 0.6),
+            Material::Gas => Color::srgba(0.2, 0.8, 0.1, 0.8),
+            Material::Air => Color::srgba(0.0, 0.0, 0.0, 0.0),
+            Material::Water => Color::srgba(0.0, 0.0, 1.0, 0.8),
+            Material::Sand => Color::srgba(1.0, 1.0, 0.1, 0.8),
+            Material::Rock => Color::srgba(1.0, 1.0, 1.0, 0.6),
         }
     }
 }
@@ -199,17 +231,17 @@ mod tests {
             width: 3,
             height: 3,
             grid: vec![
-                Tile::Air, Tile::Air, Tile::Air,
-                Tile::Water, Tile::Air, Tile::Air,
-                Tile::Rock, Tile::Rock, Tile::Rock,
-            ],
+                Material::Air, Material::Air, Material::Air,
+                Material::Water, Material::Air, Material::Air,
+                Material::Rock, Material::Rock, Material::Rock,
+            ].iter().map(|&m| Particle {material:m}).collect(),
             order: (0..9).collect()
         };
 
         sim.update();
 
-        assert_eq!(sim.grid[3], Tile::Air); // Water should fall
-        assert_eq!(sim.grid[0], Tile::Water); // Water should now be at index 0
+        assert_eq!(sim.grid[3].material, Material::Air); // Water should fall
+        assert_eq!(sim.grid[0].material, Material::Water); // Water should now be at index 0
     }
 
     #[test]
@@ -218,10 +250,10 @@ mod tests {
             width: 3,
             height: 3,
             grid: vec![
-                Tile::Rock, Tile::Rock, Tile::Rock,
-                Tile::Sand, Tile::Water, Tile::Air,
-                Tile::Air, Tile::Air, Tile::Air,
-            ],
+                Material::Rock, Material::Rock, Material::Rock,
+                Material::Sand, Material::Water, Material::Air,
+                Material::Air, Material::Air, Material::Air,
+            ].iter().map(|&m| Particle {material:m}).collect(),
             order: (0..9).collect()
         };
 
@@ -229,8 +261,8 @@ mod tests {
             sim.update();
         }
 
-        assert_eq!(sim.grid[1], Tile::Sand); // Sand should fall on water
-        assert_eq!(sim.grid[3], Tile::Air); // Sand should move down
+        assert_eq!(sim.grid[1].material, Material::Sand); // Sand should fall on water
+        assert_eq!(sim.grid[3].material, Material::Air); // Sand should move down
     }
 
     #[test]
@@ -239,15 +271,15 @@ mod tests {
             width: 3,
             height: 3,
             grid: vec![
-                Tile::Air, Tile::Air, Tile::Air,
-                Tile::Sand, Tile::Rock, Tile::Air,
-                Tile::Rock, Tile::Rock, Tile::Rock,
-            ],
+                Material::Air, Material::Air, Material::Air,
+                Material::Sand, Material::Rock, Material::Air,
+                Material::Rock, Material::Rock, Material::Rock,
+            ].iter().map(|&m| Particle {material:m}).collect(),
             order: (0..9).collect()
         };
 
         sim.update();
 
-        assert_eq!(sim.grid[2], Tile::Sand); // Sand should move to the right
+        assert_eq!(sim.grid[2].material, Material::Sand); // Sand should move to the right
     }
 }
